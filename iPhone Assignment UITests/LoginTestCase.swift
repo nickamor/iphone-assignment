455b43c25d0a352a3f338d0da83ff0a856bf8dd4
Nicholas Amor <nick.amor@gmail.com>
2016-04-10 14:59:36 +1000

    Create test framework

---
+++ iPhone Assignment UITests/LoginTestCase.swift
@@ -0,0 +1,47 @@
+//
+//  LoginTestCase.swift
+//  iPhone Assignment
+//
+//  Created by Nicholas Amor on 10/04/2016.
+//  Copyright © 2016 Nicholas Amor. All rights reserved.
+//
+
+import XCTest
+
+class LoginTestCase: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let app = XCUIApplication()
+        let usernameTextField = app.textFields["Username"]
+        usernameTextField.tap()
+        usernameTextField.typeText("bob")
+        
+        let passwordTextField = app.textFields["Password"]
+        passwordTextField.tap()
+        passwordTextField.typeText("password")
+        app.buttons["Log in"].tap()
+        
+    }
+    
+}
